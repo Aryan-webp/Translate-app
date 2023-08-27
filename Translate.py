684aff8aac9681f8867dc5d08cd0d0400f719f50
Aryan._.Singh <124171378+Aryan-webp@users.noreply.github.com>
2023-08-28 00:50:03 +0530

    Update Translate.py

--- Translate.py
+++ Translate.py
@@ -16,6 +16,11 @@ d = {'af': 'afrikaans', 'sq': 'albanian', 'am': 'amharic', 'ar': 'arabic', 'hy':
 
 hide_streamlit_style = """
             <style>
+            .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+            .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+            .viewerBadge_text__1JaDK {
+                display: none;
+            }
             #MainMenu {visibility: hidden;}
             footer {visibility: hidden;}
             </style>
